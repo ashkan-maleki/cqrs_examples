6a66d22b3dd63fa61386c82039e818e062038176
Mamal Maleki <mohammadalimalekie@gmail.com>
2022-12-27 15:48:23 +0330

    Develop data access layer

---
+++ src/TimCorey/MediatRDemo/MediatR.Lib/DataAccess/DataAccess.cs
@@ -0,0 +1,32 @@
+﻿using MediatR.Lib.Models;
+
+namespace MediatR.Lib.DataAccess;
+
+public class DataAccess : IDataAccess
+{
+    private List<PersonModel> _people = new();
+
+    public DataAccess()
+    {
+        _people.Add(new PersonModel {Id = 1, FirstName = "Johnny", LastName = "Cage"});
+        _people.Add(new PersonModel {Id = 2, FirstName = "Rachel", LastName = "Green"});
+        _people.Add(new PersonModel {Id = 3, FirstName = "Monica", LastName = "Geller"});
+        _people.Add(new PersonModel {Id = 4, FirstName = "Ross", LastName = "Geller"});
+    }
+    
+    public List<PersonModel> People
+    {
+        get => _people;
+    }
+
+    public PersonModel InsertPerson(string firstName, string lastName)
+    {
+        PersonModel personModel = new()
+        {
+            FirstName = firstName, LastName = lastName,
+            Id = _people.Max(p => p.Id) + 1
+        };
+        _people.Add(personModel);
+        return personModel;
+    }
+}
---
+++ src/TimCorey/MediatRDemo/MediatR.Lib/DataAccess/IDataAccess.cs
@@ -0,0 +1,9 @@
+﻿using MediatR.Lib.Models;
+
+namespace MediatR.Lib.DataAccess;
+
+public interface IDataAccess
+{
+    List<PersonModel> People { get; }
+    PersonModel InsertPerson(string firstName, string lastName);
+}
