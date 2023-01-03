684203791f9de4c120638e5eabddb0bdc84afca8
Mamal Maleki <mohammadalimalekie@gmail.com>
2023-01-03 21:19:27 +0330

    Add insert person command and handler

---
+++ src/TimCorey/MediatRDemo/MediatR.Lib/Commands/InsertPersonCommandClass.cs
@@ -0,0 +1,17 @@
+﻿using MediatR.Lib.Models;
+
+namespace MediatR.Lib.Commands;
+
+public record InsertPersonCommand(string FirstName, string LastName) : IRequest<PersonModel>;
+
+// public class InsertPersonCommand
+// {
+//     public InsertPersonCommand(string? firstName, string? lastName)
+//     {
+//         FirstName = firstName;
+//         LastName = lastName;
+//     }
+//
+//     public string? FirstName { get; set; }
+//     public string? LastName { get; set; }
+// }
---
+++ src/TimCorey/MediatRDemo/MediatR.Lib/Handlers/InsertPersonHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR.Lib.Commands;
+using MediatR.Lib.DataAccess;
+using MediatR.Lib.Models;
+
+namespace MediatR.Lib.Handlers;
+
+public class InsertPersonHandler : IRequestHandler<InsertPersonCommand, PersonModel>
+{
+    private readonly IDataAccess _data;
+
+    public InsertPersonHandler(IDataAccess data) => _data = data;
+
+    public Task<PersonModel> Handle(InsertPersonCommand request, CancellationToken cancellationToken) =>
+        Task.FromResult(_data.InsertPerson(request.FirstName, request.LastName));
+}
