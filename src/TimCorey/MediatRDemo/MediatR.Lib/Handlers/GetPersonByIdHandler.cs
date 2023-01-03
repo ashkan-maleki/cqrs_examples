88e86dbc9fa26f7fb56b3632df4288206b174712
Mamal Maleki <mohammadalimalekie@gmail.com>
2023-01-03 21:18:11 +0330

    Add get person by Id handler and query

---
+++ src/TimCorey/MediatRDemo/MediatR.Lib/Handlers/GetPersonByIdHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR.Lib.Models;
+using MediatR.Lib.Queries;
+
+namespace MediatR.Lib.Handlers;
+
+public class GetPersonByIdHandler : IRequestHandler<GetPersonByIdQuery, PersonModel?>
+{
+    private readonly IMediator _mediator;
+
+    public GetPersonByIdHandler(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    public async Task<PersonModel?> Handle(GetPersonByIdQuery request, CancellationToken cancellationToken)
+    {
+        List<PersonModel> personList = await _mediator.Send(new GetPersonListQuery());
+        PersonModel? person = personList.FirstOrDefault(x => x.Id == request.Id);
+
+        return person;
+    }
+}
---
+++ src/TimCorey/MediatRDemo/MediatR.Lib/Queries/GetPersonByIdQuery.cs
@@ -0,0 +1,5 @@
+﻿using MediatR.Lib.Models;
+
+namespace MediatR.Lib.Queries;
+
+public record GetPersonByIdQuery(int Id) : IRequest<PersonModel>;
